7ece3726f748a22e480dc1a98f12e201d22cbd01
Sean Humeniuk <seanhumeniuk@gmail.com>
2015-06-27 18:53:56 -0400

    Add Ranges Filter UI test

--- app/src/androidTest/java/com/github/sean_h/paintmanager/PaintListActivityTest.java
+++ app/src/androidTest/java/com/github/sean_h/paintmanager/PaintListActivityTest.java
@@ -1,11 +1,19 @@
 package com.github.sean_h.paintmanager;
 
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.action.ViewActions;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasToString;
 
 public class PaintListActivityTest extends ActivityInstrumentationTestCase2<PaintListActivity> {
     private PaintListActivity mActivity;
@@ -19,6 +27,7 @@ public class PaintListActivityTest extends ActivityInstrumentationTestCase2<Pain
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
         mActivity = getActivity();
         mRangesSpinner = (Spinner)mActivity.findViewById(R.id.ranges_spinner);
         mPaintListView = (ListView)mActivity.findViewById(R.id.paint_list);
@@ -35,6 +44,26 @@ public class PaintListActivityTest extends ActivityInstrumentationTestCase2<Pain
         assertTrue(mRangesSpinner.getCount() > 0);
         String firstItem = (String)mRangesSpinner.getItemAtPosition(0);
         assertTrue(mActivity.getString(R.string.range_spinner_default).equals(firstItem));
+    }
+
+    public void testRangesSpinnerFilter() {
+        int paintCountWithoutFilter = mPaintListView.getCount();
+        onView(withId(R.id.ranges_spinner))
+                .perform(ViewActions.click());
+        onData(hasToString(equalTo("Primary Colors")))
+                .perform(ViewActions.click());
+        int paintCountWithFilter = mPaintListView.getCount();
+        assertTrue(paintCountWithFilter < paintCountWithoutFilter);
+
+        List<String> paintNames = new ArrayList<>();
+        for (int i = 0; i < mPaintListView.getCount(); i++) {
+            Paint paint = (Paint)mPaintListView.getItemAtPosition(i);
+            paintNames.add(paint.name);
+        }
+
+        assertTrue(paintNames.contains("Red"));
+        assertTrue(paintNames.contains("Blue"));
+        assertFalse(paintNames.contains("Green"));
     }
 
     public void testPaintStatusSpinner() {
